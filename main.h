bb50eb0979c8426d85a16a61eed154091d54ac2b
guenoel <guenooel@hotmail.com>
2022-07-12 17:55:49 +0200

    Task 0 Printf

--- main.h
+++ main.h
@@ -6,15 +6,21 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
-typedef struct matchs { 
-	char form; 
-	int (*f)(va_list args); 
+/**
+ * struct matchs - Listes des specifier connus
+ * @form: la lettre du specifier
+ * @f: le pointeur de fonction
+ */
+typedef struct matchs
+{
+	char form;
+	int (*f)(va_list args);
 } match;
 
 int c_end_s(char *s);
 int _count(const char *c);
 int _putchar(char c);
-int _putstring(char* s);
+int _putstring(char *s);
 int _printf(const char *format, ...);
 int printchar(va_list args);
 int printstring(va_list args);
